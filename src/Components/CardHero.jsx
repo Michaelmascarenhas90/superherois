b5d98360c562cb433c8273f1c8a507c872c85d60
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-11-17 00:21:00 -0300

    criando cards para cada personagem

---
+++ src/Components/CardHero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card, CardMedia } from '@material-ui/core';
+
+const CardHero = () => {
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        alt={`imagem do super ${data.name}`}
+        image={data.images.md}
+      >
+      
+      </CardMedia>
+    </Card>
+  );
+}
